9cb805147abf972eed4dd9d58946d2a077f04dee
sadiqss <sadiq.sau48@gmail.com>
2022-04-10 00:29:10 +0100

    toogle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import app from "./firebase.init";
@@ -9,6 +9,7 @@ const auth = getAuth(app)
 
 function App() {
   const [validated, setValidated] = useState(false);
+  const [registered, setRegistered] = useState(false);
   const [error, setError] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -18,6 +19,11 @@ function App() {
   const handlePasswordBlur = event => {
     setPassword(event.target.value);
   }
+
+  const handleRegisteredChange = event => {
+    setRegistered(event.target.checked)
+  }
+
   const handleFormSubmit = event => {
     event.preventDefault();
     const form = event.currentTarget;
@@ -32,21 +38,40 @@ function App() {
 
     setValidated(true);
     setError('');
-    createUserWithEmailAndPassword(auth, email, password)
-      .then(result => {
-        const user = result.user;
-        console.log(user);
-      })
-      .catch(error => {
-        console.log(error);
-      })
+
+    if (registered) {
+      signInWithEmailAndPassword(auth, email, password)
+        .then(result => {
+          const user = result.user;
+          console.log(user);
+        })
+        .catch(error => {
+          console.log(error);
+          setError(error.message);
+        })
+    }
+    else {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then(result => {
+          const user = result.user;
+          console.log(user);
+          setEmail('');
+          setPassword('');
+        })
+        .catch(error => {
+          console.log(error);
+          setError(error.message);
+        })
+    }
+
+
     // console.log('form submitted', email, password)
     event.preventDefault();
   }
   return (
     <div>
       <div className="registration w-50 mx-auto mt-5">
-        <h2 className="text-primary">Please Register</h2>
+        <h2 className="text-primary">Please {registered ? 'Login' : 'Register'}</h2>
         <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
@@ -67,9 +92,12 @@ function App() {
               Please provide valid password.
             </Form.Control.Feedback>
           </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check onChange={handleRegisteredChange} type="checkbox" label="Already Registered?" />
+          </Form.Group>
           <p className="text-danger">{error}</p>
           <Button variant="primary" type="submit">
-            Submit
+            {registered ? 'Login' : 'Register'}
           </Button>
         </Form>
       </div>
